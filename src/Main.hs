d831d5be8bcbbc16d0583305b4076010c27878be
James Porter <james@jamesporter.me>
2017-01-22 15:34:05 -0500

    wip parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,32 @@
 module Main where
 
+import Control.Monad
+import Text.Megaparsec
+import Text.Megaparsec.String
+import Text.Megaparsec.Lexer
+
+data Tok =
+  TBlock Int String |
+  TChar Char
+
+instance Show Tok where
+  show (TChar c) = show c
+  show (TBlock i s) = join $ replicate i s
+
+int :: Parser Int
+int = fromIntegral <$> integer
+
+parseChar :: Parser Tok
+parseChar = TChar <$> lowerChar
+
+parseBlock :: Parser Tok
+parseBlock = TBlock <$> int <*> between (char '[') (char ']') (many lowerChar)
+
+parseTok :: Parser Tok
+parseTok = parseChar <|> parseBlock
+
+parseInput :: Parser [Tok]
+parseInput = many parseTok
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = putStrLn "hello"
